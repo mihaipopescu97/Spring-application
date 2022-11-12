5df12710ff0cfe32b4c989604fb2c61e881dfb74
mihaipopescu97 <mihaipopescu97@yahoo.com>
2022-11-12 16:23:38 +0200

    WS-2

---
+++ src/main/java/com/example/demo/controllers/StoreController.java
@@ -0,0 +1,9 @@
+package com.example.demo.controllers;
+
+/**
+ * @author : Mihai-Cristian Popescu
+ * @since : 11/12/2022, Sat
+ **/
+
+public class StoreController {
+}
---
+++ src/main/java/com/example/demo/entities/Store.java
@@ -0,0 +1,15 @@
+package com.example.demo.entities;
+
+import java.util.List;
+
+/**
+ * @author : Mihai-Cristian Popescu
+ * @since : 11/12/2022, Sat
+ **/
+
+public class Store {
+
+    private List<Product> products;
+
+    private List<User> customers;
+}
