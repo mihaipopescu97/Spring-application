71b1a2035cea9c392b30e57b5dc5e06532292714
mihaipopescu97 <49340373+mihaipopescu97@users.noreply.github.com>
2022-11-12 16:41:37 +0200

    WS-2 (#1)
    WS-2

---
+++ src/main/java/com/example/demo/controllers/ProductController.java
@@ -0,0 +1,11 @@
+package com.example.demo.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : Mihai-Cristian Popescu
+ * @since : 11/12/2022, Sat
+ **/
+@RestController
+public class ProductController {
+}
---
+++ src/main/java/com/example/demo/entities/User.java
@@ -0,0 +1,28 @@
+package com.example.demo.entities;
+
+/**
+ * @author : Mihai-Cristian Popescu
+ * @since : 11/12/2022, Sat
+ **/
+
+public class User {
+
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
